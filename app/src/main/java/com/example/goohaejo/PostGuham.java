fa5973adf8e8f58412cf97d01c9c8bbca0244aed
soo4767 <soo4767@naver.com>
2021-02-24 16:15:52 +0900

    creat app

---
+++ app/src/main/java/com/example/goohaejo/PostGuham.java
@@ -0,0 +1,14 @@
+package com.example.goohaejo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PostGuham extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_guham);
+    }
+}
